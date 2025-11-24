419f83ef0d8266b823cdc2e8f527b99beba55b39
MakerByNight <2565361+mshafer1@users.noreply.github.com>
2025-11-23 19:25:17 -0600

    move sort and limit into find directly to let mongo optimize

--- app/backend/up_mate_backend/_db/__init__.py
+++ app/backend/up_mate_backend/_db/__init__.py
@@ -17,13 +17,8 @@ def add_status(info: dict):
     _collection.insert_one({**info, "timestamp":timestamp.astimezone()})
 
 def get_latest_status(user: str):
-    newest = _collection.find({"user": user}, {"_id": False}).sort("timestamp", pymongo.DESCENDING).limit(1).to_list()
+    newest = _collection.find({"user": user}, {"_id": False}, sort=[('timestamp', pymongo.DESCENDING)], limit=1).to_list()
     if not newest:
         raise NotFoundError()
     
     return newest[0]
-
-if __name__ == '__main__':
-    add_status(
-        dict(user="John Doe", pain_level=3.2, notes="Eh, ok today")
-    )
